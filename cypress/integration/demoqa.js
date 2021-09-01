fcfe5c07592c77819a3e95384ce83ad506384387
jenar-k <jenarkuswidiardi@gmail.com>
2021-09-01 21:29:19 +0700

    commit kelima

--- cypress/integration/demoqa.js
+++ cypress/integration/demoqa.js
@@ -1,31 +1,30 @@
-describe('Demo QA', function () {
-    it('TC-01', function () {
-        cy.visit('https://google.com');
-        // cy.visit('https://demoqa.com/automation-practice-form');
-        // cy.wait(10000)
-        // cy.url().should('include', 'demoqa.com');
-        // cy.get('#firstName').type('Jenar').should('have.value', 'Jenar');
-        // cy.get('#lastName').type('JenarLagi').should('have.value', 'JenarLagi');
-        // cy.get('#userEmail').type('jenar@email.com').should('have.value', 'jenar@email.com');
-        // cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click()
-        // cy.get('#userNumber').type('080989999').should('have.value', '080989999');
+describe('Admin PKH', function () {
+    it('TC-01 Admin PKH Coba Upload', function () {
+        cy.mainURL();
+        //cy.visit('https://demoqa.com/automation-practice-form');
+        cy.url().should('include', 'demoqa.com');
+        cy.get('#firstName').type('Jenar').should('have.value', 'Jenar');
+        cy.get('#lastName').type('JenarLagi').should('have.value', 'JenarLagi');
+        cy.get('#userEmail').type('jenar@email.com').should('have.value', 'jenar@email.com');
+        cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click()
+        cy.get('#userNumber').type('080989999').should('have.value', '080989999');
         // cy.get('#dateOfBirthInput' , {force : true}).click().type('{selectall}' , '{backspace}').type('13 Jan 2000').type('{enter}');
-        // cy.get('#dateOfBirthInput').invoke('val','13 Jan 2000');
-        // cy.get('#dateOfBirthInput').should('have.value', '13 Jan 2000');
-        // cy.get('.subjects-auto-complete__value-container',{force:true}).type('daftar').should('have.value', '');
-        // cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click();
-        // cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(2) > .custom-control-label').click();
+        cy.get('#dateOfBirthInput').invoke('val','13 Jan 2000');
+        cy.get('#dateOfBirthInput').should('have.value', '13 Jan 2000');
+        cy.get('.subjects-auto-complete__value-container',{force:true}).type('daftar').should('have.value', '');
+        cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click();
+        cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(2) > .custom-control-label').click();
         // cy.get('#uploadPicture').attachFile('testgambar.jpg');
-        // cy.get('#currentAddress').type('Ngaglik, Yogyakarta').should('have.value', 'Ngaglik, Yogyakarta');
-        // cy.get('.css-yk16xz-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').should('not.be.disabled');
-        // cy.get('#stateCity-wrapper > :nth-child(2)').should('not.be.disabled');
-        // cy.get('#stateCity-wrapper > :nth-child(3)').should('not.be.disabled');
-        // cy.get('#state > .css-yk16xz-control > .css-1hwfws3 > .css-1wa3eu0-placeholder').click();
-        // cy.get('#react-select-3-option-2').click();
-        // cy.get('#city > .css-yk16xz-control > .css-1hwfws3').click();
-        // cy.get('#react-select-4-option-0').click();
-        // cy.get('#submit').click();
-        // cy.get('#example-modal-sizes-title-lg').should('have.text', 'Thanks for submitting the form');
-        // cy.get('tbody > :nth-child(10) > :nth-child(2)').should('have.text', 'Haryana Karnal')
+        cy.get('#currentAddress').type('Ngaglik, Yogyakarta').should('have.value', 'Ngaglik, Yogyakarta');
+        //cy.get('.css-yk16xz-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').should('not.be.disabled');
+        cy.get('#stateCity-wrapper > :nth-child(2)').should('not.be.disabled');
+        cy.get('#stateCity-wrapper > :nth-child(3)').should('not.be.disabled');
+        cy.get('#state > .css-yk16xz-control > .css-1hwfws3 > .css-1wa3eu0-placeholder').click();
+        cy.get('#react-select-3-option-2').click();
+        cy.get('#city > .css-yk16xz-control > .css-1hwfws3').click();
+        cy.get('#react-select-4-option-0').click();
+        cy.get('#submit').click();
+        cy.get('#example-modal-sizes-title-lg').should('have.text', 'Thanks for submitting the form');
+        cy.get('tbody > :nth-child(10) > :nth-child(2)').should('have.text', 'Haryana Karnal')
     })
 })
--- cypress/integration/uploadpkh.js
+++ cypress/integration/uploadpkh.js
@@ -5,8 +5,8 @@ describe('Admin PKH', function () {
         cy.get('#input-password').type('admin');
         cy.get('#btn-login').click();
         // cy.get('#dataTable_filter > label > .form-control').type('jenar');
-        cy.get('.d-flex > .btn').click();
-        //cy.get('#drop-zone').attachFile('testgambar.jpg');
-        cy.get('input[type="file"]').attachFile('testgambar.jpg');
+        // cy.get('.d-flex > .btn').click();
+        // cy.get('#drop-zone').attachFile('testgambar.jpg');
+        // cy.get('input[type="file"]').attachFile('testgambar.jpg');
     })
 })
