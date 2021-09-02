b6f1303308ecdb36dfd1d78511c8d7880b02d624
jenar-k <jenarkuswidiardi@gmail.com>
2021-09-02 18:40:54 +0700

    commit kedelapan

--- cypress/integration/demoqa.js
+++ cypress/integration/demoqa.js
@@ -1,5 +1,5 @@
-describe('Admin PKH', function () {
-    it('TC-01 Admin PKH Coba Upload', function () {
+describe('Demo QA', function () {
+    it('TC-01', function () {
         cy.visit('https://demoqa.com/automation-practice-form');
         cy.url().should('include', 'demoqa.com');
         cy.get('#firstName').type('Jenar').should('have.value', 'Jenar');
@@ -13,9 +13,9 @@ describe('Admin PKH', function () {
         cy.get('.subjects-auto-complete__value-container',{force:true}).type('daftar').should('have.value', '');
         cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click();
         cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(2) > .custom-control-label').click();
-        cy.get('#uploadPicture').attachFile('testgambar.jpg');
+        // cy.get('#uploadPicture').attachFile('testgambar.jpg');
         cy.get('#currentAddress').type('Ngaglik, Yogyakarta').should('have.value', 'Ngaglik, Yogyakarta');
-        //cy.get('.css-yk16xz-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').should('not.be.disabled');
+        //c y.get('.css-yk16xz-control > .css-1wy0on6 > .css-tlfecz-indicatorContainer').should('not.be.disabled');
         cy.get('#stateCity-wrapper > :nth-child(2)').should('not.be.disabled');
         cy.get('#stateCity-wrapper > :nth-child(3)').should('not.be.disabled');
         cy.get('#state > .css-yk16xz-control > .css-1hwfws3 > .css-1wa3eu0-placeholder').click();
