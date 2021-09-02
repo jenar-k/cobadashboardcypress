2566b3fe5ce9fefbb5d83a5b9704eda8ada63a39
jenar-k <jenarkuswidiardi@gmail.com>
2021-09-02 18:46:31 +0700

    commit kesembilan

--- cypress/integration/uploadpkh.js
+++ cypress/integration/uploadpkh.js
@@ -4,9 +4,9 @@ describe('Admin PKH', function () {
         cy.get('#input-email').type('e@dojobox.id');
         cy.get('#input-password').type('admin');
         cy.get('#btn-login').click();
-        // cy.get('#dataTable_filter > label > .form-control').type('jenar');
-        // cy.get('.d-flex > .btn').click();
-        // cy.get('#drop-zone').attachFile('testgambar.jpg');
-        // cy.get('input[type="file"]').attachFile('testgambar.jpg');
+        cy.get('#dataTable_filter > label > .form-control').type('jenar');
+        cy.get('.d-flex > .btn').click();
+        cy.get('#drop-zone').attachFile('testgambar.jpg');
+        cy.get('input[type="file"]').attachFile('testgambar.jpg');
     })
 })
